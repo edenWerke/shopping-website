231971f725087cbe48c541d984f069b10d03469c
edenWerke <edenw6292@gmail.com>
2025-04-07 21:46:35 +0300

    shopping

--- app/components/HeaderMain.tsx
+++ app/components/HeaderMain.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BsSearch } from "react-icons/bs";
-
+import { FaRegHeart , FaShoppingBag } from 'react-icons/fa';
+import { HiOutlineShoppingBag } from "react-icons/hi";
 const HeaderMain = () => {
   return (
     <div className="border-b border-gray-200 py-6">
@@ -16,6 +17,20 @@ const HeaderMain = () => {
             className="border border-gray-300 p-2 px-4 rounded-lg w-full"
           />
           <BsSearch className='absolute right-0 top-0 m-3 text-gray-400'/>
+        </div>
+        <div className="flex items-center gap-3"> {/* Reduced gap to 2 */}
+          <div className="relative">
+            <FaRegHeart className="text-gray-600 text-[32px]" />
+            <div className="bg-red-600 rounded-full absolute top-0 right-0 w-[20px] h-[20px] text-[12px] text-white grid place-items-center translate-x-1 translate-y-1">
+              0
+            </div>
+          </div>
+          <div className="relative">
+            <HiOutlineShoppingBag  className="text-gray-600 text-[32px]" />
+            <div className="bg-red-600 rounded-full absolute top-0 right-0 w-[20px] h-[20px] text-[12px] text-white grid place-items-center translate-x-1 translate-y-1">
+              0
+            </div>
+          </div>
         </div>
       </div>
     </div>
